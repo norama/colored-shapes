c096453ab6ac7b29ddb717d40e008108093f1309
Nora Mate <norama.matema@gmail.com>
2023-12-17 13:07:12 +0100

    configurable ColoredShapeImage content

--- src/components/shapes/ColoredShape.tsx
+++ src/components/shapes/ColoredShape.tsx
@@ -1,7 +1,10 @@
 "use client"
 
+import { ListItemButton } from "@mui/material"
+import { Edit as EditIcon } from "@mui/icons-material"
 import ColoredShapeImage from "./ColoredShapeImage"
 import { useGetColoredShape } from "@store/useShapes"
+import { useState } from "react"
 
 interface ColoredShapeProps {
   id: number
@@ -11,12 +14,21 @@ interface ColoredShapeProps {
 const ColoredShape = ({ id, size = 112 }: ColoredShapeProps) => {
   const getColoredShape = useGetColoredShape()
   const coloredShape = getColoredShape(id)
+  const [content, setContent] = useState<React.ReactNode>("")
 
   if (!coloredShape) {
     throw new Error(`Colored Shape (${id}) is not found.`)
   }
 
-  return <ColoredShapeImage coloredShape={coloredShape} size={size} />
+  return (
+    <ListItemButton
+      sx={{ width: "fit-content" }}
+      onMouseEnter={() => setContent(<EditIcon />)}
+      onMouseLeave={() => setContent("")}
+    >
+      <ColoredShapeImage coloredShape={coloredShape} content={content} size={size} />
+    </ListItemButton>
+  )
 }
 
 export default ColoredShape
--- src/components/shapes/ColoredShapeImage.tsx
+++ src/components/shapes/ColoredShapeImage.tsx
@@ -1,17 +1,19 @@
 import { Avatar } from "@mui/material"
 import { TColoredShape } from "@ts/shapes"
+import React from "react"
 
 interface ColoredShapeImageProps {
   coloredShape: TColoredShape
+  content?: React.ReactNode
   size?: number
 }
 
-const ColoredShapeImage = ({ coloredShape, size = 56 }: ColoredShapeImageProps) => {
+const ColoredShapeImage = ({ coloredShape, content = "", size = 56 }: ColoredShapeImageProps) => {
   const { color, shape } = coloredShape
 
   return (
     <Avatar sx={{ width: size, height: size, background: color }} variant={shape}>
-      &nbsp;
+      {content}
     </Avatar>
   )
 }
