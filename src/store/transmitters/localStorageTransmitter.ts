19c56620bb3def3849c589211e9303753d7be087
Nora Mate <norama.matema@gmail.com>
2023-12-20 12:36:04 +0100

    localStorageTransmitter

--- src/store/transmitters/localStorageTransmitter.ts
+++ src/store/transmitters/localStorageTransmitter.ts
@@ -1,21 +1,46 @@
-import { addColoredShape, removeColoredShape, updateColoredShape } from "store/shapes"
+import {
+  $coloredShapes,
+  addColoredShape,
+  removeColoredShape,
+  updateColoredShape,
+} from "store/shapes"
 import { emitter, stopTransmitter } from "transmitter"
+import { TColoredShape } from "types"
+
+const STORAGE_KEY = "coloredShapes"
+
+const load = () => {
+  const coloredShapesString = localStorage.getItem(STORAGE_KEY)
+  if (coloredShapesString) {
+    $coloredShapes.set(JSON.parse(coloredShapesString) as TColoredShape[])
+  }
+}
+
+const save = () => {
+  const coloredShapes = $coloredShapes.get()
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(coloredShapes))
+}
 
 export const startTransmitter = () => {
   stopTransmitter()
 
+  load()
+
   emitter.on("add", () => {
     console.log("ADD")
     addColoredShape()
+    save()
   })
 
   emitter.on("remove", (id) => {
     console.log("REMOVE")
     removeColoredShape(id)
+    save()
   })
 
   emitter.on("update", (id, coloredShape) => {
     console.log("UPDATE")
     updateColoredShape(id, coloredShape)
+    save()
   })
 }
